cef78b167bedc84411af62508281c9a2b6e32050
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-09-04 10:40:35 +0900

    Add MemberRepositoryTest

--- src/test/java/jpabook/springbootjpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/springbootjpashop/MemberRepositoryTest.java
@@ -1,11 +1,14 @@
 package jpabook.springbootjpashop;
 
 import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
@@ -17,14 +20,24 @@ public class MemberRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
+    @Transactional
+    @Rollback(false) //test는 테스트가끝나면 자동으로 데이터를 롤백해서 db에 남지않는데, false해주면 남음
     public void testMember() throws Exception {
-        //given
+
         Member member = new Member();
         member.setUsername("Junseok");
-        MemberRepository
 
-        //when
+        Long savedId = memberRepository.save(member);
 
-        //then
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        
+        //객체끼리의 equals는 주소값을 비교하므로 원래라면 false가 나오지만,
+        //jpa는 영속성컨텍스트를 통해 비교하므로 같은 id값이면 true가 나옴
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        //그래서 findMember를통해 3가지 비교를 했지만, select쿼리조차 안나감
     }
 }
