3b7e60d5b0e531823448c122b406f0710933f250
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-09-06 16:42:52 +0900

    Update Address.class to use constructor

--- src/main/java/jpabook/springboot_jpashop/domain/Address.java
+++ src/main/java/jpabook/springboot_jpashop/domain/Address.java
@@ -10,4 +10,17 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    //실무에서 Setter를 남발하게되면, 유지보수가 3~4배 힘들어지게된다.
+    //그래서 웬만하면 Getter만 열어두고, 나머지는 생성자를통해 새로 생성하는 방식으로 해야한다.
+
+    //jpa특성상 기본생성자가없으면 동작하지않으므로 만들어주고, protected를 걸어두면 외부에서 사용이 불가능하다
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
