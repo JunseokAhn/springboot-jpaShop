dca81fe0813dfd4d39671cd883777e6e87610f0b
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-10-03 06:36:00 +0900

    Add updateItemForm.html

--- src/main/java/jpabook/springboot_jpashop/controller/ItemController.java
+++ src/main/java/jpabook/springboot_jpashop/controller/ItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -37,9 +39,40 @@ public class ItemController {
     }
 
     @GetMapping("/items/itemList")
-    public String itemList(Model model){
+    public String itemList(Model model) {
         List<Item> itemList = itemService.findAll();
         model.addAttribute("items", itemList);
         return "items/itemList";
+    }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+        BookForm form = new BookForm();
+
+        form.setName(item.getName());
+        form.setAuthor(item.getAuthor());
+        form.setId(itemId);
+        form.setIsbn(item.getIsbn());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+
+        model.addAttribute("form", form);
+
+        return "items/updateItemForm";
+    }
+
+
+    @PostMapping("items/{itemId}/edit")
+    public String updateItemForm2(@ModelAttribute("form") BookForm form) {
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setIsbn(form.getIsbn());
+        book.setAuthor(form.getAuthor());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+
+        itemService.saveItem(book);
+        return "redirect:/items/itemList";
     }
 }
